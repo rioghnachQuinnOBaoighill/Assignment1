f7e32fc202fea2b21d164104e162e2fc3c3a3d07
rioghnachQuinnOBaoighill <r.quinnobaoighill1@nuigalway.ie>
2018-10-30 20:58:23 +0100

    Fixed CourseProgramme

--- src/main/java/ie/nuigalway/assignment1/CourseProgramme.java
+++ src/main/java/ie/nuigalway/assignment1/CourseProgramme.java
@@ -41,28 +41,34 @@ public class CourseProgramme {
         ArrayList<Student> allStudents = new ArrayList<Student>();
         ArrayList<Modules> allModules = new ArrayList<Modules>();
         allModules = this.getModules();
-        for (int i=0; i<allModules.size(); i++)
-        {
-            Modules mod = allModules.get(i);
-            ArrayList<Student> modStudents = new ArrayList<Student>();
-            modStudents = mod.getStudents();
-            
-            for (int j = 0; j<modStudents.size();j++)
-            {
-                Student stu = modStudents.get(j);
-                
-                if (stu.getCourse().equals(this.getCourseName()) &&! (allStudents.contains(stu)))
-                {
-                    allStudents.add(stu);
-                    System.out.println("Name: " + stu.getName() + "\t Course Title: " + stu.getCourse());
-                    System.out.println("\t Course " + stu.getCourse() + " Modules: ");
-                    for(int m=0; m<allModules.size(); m++)
-                    {
-                        System.out.println(allModules.get(m).getName());
-                    }
-                    System.out.println("------------------------------------------------");
-                }
-            }
+//        for (int i=0; i<allModules.size(); i++)
+//        {
+//            Modules mod = allModules.get(i);
+//            ArrayList<Student> modStudents = new ArrayList<Student>();
+//            modStudents = mod.getStudents();
+//            
+//            for (int j = 0; j<modStudents.size();j++)
+//            {
+//                Student stu = modStudents.get(j);
+//                
+//                if (stu.getCourse().equals(this.getCourseName()) &&! (allStudents.contains(stu)))
+//                {
+//                    allStudents.add(stu);
+//                    System.out.println("Name: " + stu.getName() + "\t Course Title: " + stu.getCourse());
+//                    System.out.println("\t Course " + stu.getCourse() + " Modules: ");
+//                    for(int m=0; m<allModules.size(); m++)
+//                    {
+//                        System.out.println(allModules.get(m).getName());
+//                    }
+//                    System.out.println("------------------------------------------------");
+//                }
+//            }
+//        }
+    for (Modules mod : allModules){
+        ArrayList<Student> modStudents = mod.getStudents();
+        for (Student student : modStudents){
+            System.out.println(student.getName());
+        }
         }
     }
 }
